92344194fd8fce4fac48499b15516dd212820140
Joana <joanajanefdez@gmail.com>
2022-07-07 10:56:09 +0200

    Ej 5

---
+++ src/U5_EJ5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class U5_EJ5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce un numero:");
+		int numIntro = sc.nextInt();
+		sc.close();
+		
+		if(numIntro%2==0) {
+			System.out.println("Es divisible entre 2");
+		}else {
+			System.out.println("No es divisible entre 2");
+		}
+
+	}
+
+}
