43108acaf328ac4678aef99804baec54e4639dce
Joana <joanajanefdez@gmail.com>
2022-07-07 10:14:31 +0200

    1r ej

--- src/U5_EJ1App.java
+++ src/U5_EJ1App.java
@@ -2,7 +2,11 @@
 public class U5_EJ1App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num1 = 4;
+		int num2 = 7;
+
+		System.out.println("El numero mayor es: " 
+							+ Math.max(num1, num2));
 
 	}
 
