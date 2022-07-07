7bc1be91571e31053c8bd28645e05ea69b071bb4
Joana <joanajanefdez@gmail.com>
2022-07-07 10:15:42 +0200

    1r ej - prueba cambio valor

--- src/U5_EJ1App.java
+++ src/U5_EJ1App.java
@@ -2,8 +2,8 @@
 public class U5_EJ1App {
 
 	public static void main(String[] args) {
-		int num1 = 4;
-		int num2 = 7;
+		int num1 = 10;
+		int num2 = 3;
 
 		System.out.println("El numero mayor es: " 
 							+ Math.max(num1, num2));
